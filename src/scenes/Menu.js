10b0da5f38ac1c3de657bdd93c6523c1d102392e
Ethan Salzman <ezsalzma@ucsc.edu>
2020-04-08 15:58:49 -0700

    Scenes and Assets Implemented

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,6 +1,15 @@
-class Menu extends Phaser.scene {
+class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
+    }
+
+    create() {
+        console.log(this);
+        //display menu text
+        this.add.text(20,20,"Rocket Patrol Menu");
+
+        //launch next scene
+        this.scene.start("playScene");
     }
 
 }
