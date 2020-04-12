401abfffd31b7e2872f9d128363eb57f170e0b35
Ethan Salzman <ezsalzma@ucsc.edu>
2020-04-12 06:29:09 -0700

    Finishing game with sound, menu and difficulty

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -5,7 +5,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         //add an object to an existing scece, display list and update list
         scene.add.existing(this);
         this.isFiring = false;
-
+        this.sfxRocket = scene.sound.add('sfx_rocket'); //adds rocket sfx
     }
 
     update(){
@@ -18,8 +18,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
             }
         }
         //fire button (NOT SPACEBAR)
-        if(Phaser.Input.Keyboard.JustDown(keyF)){
+        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring){
             this.isFiring = true;
+            this.sfxRocket.play(); //play sfx
         }
         //if fired move up
         if(this.isFiring && this.y >= 108){
